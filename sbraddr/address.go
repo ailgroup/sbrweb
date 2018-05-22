bfc318ab236baf392ad48adfdbdf7576360b56da
Joshua Bowles <jbowayles@gmail.com>
2018-05-22 05:53:22 -0600

    add sabre address package

---
+++ sbraddr/address.go
@@ -0,0 +1,13 @@
+package Sbraddr
+
+// Address represents typical building addresses
+type Address struct {
+	AddressLine   string `xml:"AddressLine,omitempty"`
+	Street        string `xml:"StreetNumber,omitempty"`
+	City          string `xml:"CityName,omitempty"`
+	StateProvince struct {
+		StateCode string `xml:"StateCode,attr"`
+	} `xml:"StateCountyProv,omitempty"`
+	CountryCode string `xml:"CountryCode,omitempty"`
+	Postal      string `xml:"PostalCode,omitempty"`
+}
