58425b99aaafcfd418b1fe692205829b4d051b17
Joshua Bowles <jbowayles@gmail.com>
2018-06-22 07:03:06 -0600

    Common environment router, config, session pool
    Gelling around patterns for dependency injection with viper and
    configuration settings, as well as the chi muxer for dealing with http
    handlers

--- client/client.go
+++ client/client.go
@@ -1,18 +1,57 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/ailgroup/sbrweb/client/common"
 	"github.com/ailgroup/sbrweb/client/handlers"
-	"github.com/gorilla/mux"
+	"github.com/fsnotify/fsnotify"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/spf13/viper"
 )
 
-// registerRoutes is responsible for regisetering the server-side request handlers
-func registerRoutes(env *common.Env, r *mux.Router) {
-	r.Handle("/avail", handlers.HotelAvailHandler(env)).Methods("GET")
+func setConfig() *viper.Viper {
+	conf := viper.GetViper()
+
+	conf.SetConfigName("config")
+	conf.AddConfigPath("$HOME")
+	conf.AddConfigPath(".")
+	conf.BindEnv("SABRE_USERNAME")
+	conf.BindEnv("SABRE_PASSWORD")
+	conf.BindEnv("SABRE_PCC")
+
+	err := conf.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+	conf.WatchConfig()
+	conf.OnConfigChange(func(e fsnotify.Event) {
+		fmt.Println("Config file changed:", e.Name)
+	})
+
+	return conf
+}
+
+// registerRoutes is responsible for registering the server-side request handlers
+func registerRoutes(env *common.Env) {
+	env.Mux.Get("/", func(w http.ResponseWriter, r *http.Request) { w.Write([]byte("hi")) })
+	env.Mux.Handle("/avail", handlers.HotelAvailHandler(env))
 }
 
 func main() {
-	env := common.Env{}
-	r := mux.NewRouter()
-	registerRoutes(&env, r)
+	// deal with context??
+	r := chi.NewRouter()
+	//Logger Logs the start and end of each request with the elapsed processing time
+	r.Use(middleware.Logger)
+	// Heartbeat Monitoring endpoint to check the servers pulse
+	r.Use(middleware.Heartbeat("/heartbeat"))
+
+	env := common.Env{
+		Config: setConfig(),
+		Mux:    r,
+	}
+	registerRoutes(&env)
+	http.ListenAndServe(":8080", r)
 }
--- client/common/common.go
+++ client/common/common.go
@@ -5,11 +5,16 @@ package common
 //"github.com/isomorphicgo/isokit"
 //"github.com/isomorphicgo/isokit"
 import (
-	"github.com/julienschmidt/httprouter"
+	"github.com/ailgroup/sbrweb/engine/srvc"
+	"github.com/go-chi/chi"
+	"github.com/spf13/viper"
 )
 
 type Env struct {
-	Router *httprouter.Router
+	//Router *httprouter.Router
+	Mux         *chi.Mux
+	SessionPool *srvc.SessionPool
+	Config      *viper.Viper
 	//DStore datastore.Datastore
 	//FStore *sessions.FilesystemStore
 	//Router *isokit.Router
