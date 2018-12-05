9f01ab8e287f4ae963a56cf369c374e20bc6d6d1
Joshua Bowles <jbowayles@gmail.com>
2018-12-04 20:53:11 -0700

    begin vehicle location request

---
+++ engine/grndws/grndws.go
@@ -0,0 +1,4 @@
+/* Package grndws (Ground Transportation Web Services) implements Sabre SOAP car rental through locations, availability, rate code rules, rates non-airport, and book car reservation web services.
+ */
+
+package grndws
